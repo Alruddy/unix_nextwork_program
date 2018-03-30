9fda15f88e8e3e43582af742e04ce48a003af258
xlmeng <mxlalruddy@hotmail.com>
2018-03-30 17:20:55 +0800

    初始版本，使用线程，但会出现僵尸进程

---
+++ tcpclisev/str_cli01.c
@@ -0,0 +1,12 @@
+#include "unp.h"
+
+void str_cli(FILE *fp, int sockfd) {
+    char    recvline[MAXLINE], sendline[MAXLINE];
+    while (Fgets(sendline, MAXLINE, fp) != NULL) {
+        Writen(sockfd, sendline, strlen(sendline));
+        if(Readline(sockfd, recvline, MAXLINE) == 0) 
+            err_quit("str_echo: server terminated prematurely");
+        Fputs(recvline, stdout);
+    }
+}
+
---
+++ tcpclisev/tcpserv01.c
@@ -0,0 +1,32 @@
+#include "unp.h"
+
+int
+main(int argc, char** argv) {
+    int     listenfd, connfd;
+    pid_t   childpid;
+    struct sockaddr_in servaddr, clieaddr;
+    socklen_t   clielen;
+
+    listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(9997);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    Bind(listenfd, (SA*) &servaddr, sizeof(servaddr));
+
+    Listen(listenfd, LISTENQ);
+
+    for ( ; ; ) {
+        clielen = sizeof(clieaddr);
+        connfd = Accept(listenfd, (SA *)&clieaddr, &clielen);
+
+        if ((childpid = Fork()) == 0) {
+            Close(listenfd);
+            str_echo(connfd);
+            Close(connfd);
+            exit(0);
+        }
+        Close(connfd);
+    }
+}
